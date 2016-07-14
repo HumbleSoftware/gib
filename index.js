b6c6a1ce71740acf1092f80c4a572c542de0ccdb
Carl Sutherland <carl@humblesoftware.com>
2016-07-13 22:10:36 -0700

    Start of gib take 2.
    Move tasks to recipe modules.  First pass at gib recipe automation.

--- index.js
+++ index.js
@@ -1,8 +1,91 @@
 'use strict';
 
-var gulp = require('gulp');
+//var gulp = require('gulp');
+
+var pkgUp = require('pkg-up');
+var camelCase = require('camelcase');
 
 module.exports = {
+  autoload: autoload
+  /*
   gulp: gulp,
   gulpfile: require('./lib/gulpfile.js')
+  */
 };
+
+
+
+function autoload () {
+
+  var config = require(pkgUp.sync());
+  var searchKeys = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
+  var recipes = new Registry();
+
+  searchKeys
+    // Get packages names:
+    .reduce(function (p, searchKey) {
+      var dependencies = config[searchKey] || [];
+      return p.concat(Array.isArray(dependencies) ? dependencies : Object.keys(dependencies));
+    }, [])
+    // Filter for recipe names:
+    .filter(function (packageName) {
+      return /^gib-recipe-.*/.test(packageName);
+    })
+    // Load recipes:
+    .forEach(function (recipeName) {
+      recipes.register(recipeName, require(recipeName));
+    });
+
+  return recipes;
+}
+
+
+function Registry () {
+
+  var bus = {
+
+    /**
+     * Trigger a browser refresh.
+     */
+    refresh: function () {
+
+    },
+
+    /**
+     * Trigger a browser refresh from stream changes.
+     */
+    refreshStream: function () {
+      if (registry.server) {
+        return registry.server.refreshStream();
+      }
+    }
+  };
+
+  var registry = {
+
+  };
+
+  return {
+
+    /**
+     * Regurn registry copy.
+     */
+    all: function () {
+      return Object.assign({}, registry);
+    },
+
+    /**
+     * Register a recipe
+     */
+    register: function (name, recipe) {
+      // Fancy name:
+      // gib-recipe-fancy-pants -> fancyPants
+      name = camelCase.apply(camelCase, name.split('-').slice(2));
+      registry[name] = recipe;
+      if (typeof recipe.register === 'function') {
+        recipe.register(bus);
+      }
+    }
+  };
+}
+
